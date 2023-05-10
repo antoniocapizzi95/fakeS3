47ea601c97bd55be8344c6464083bfcc89544e3c
antonio <antoniocapizzi95@hotmail.it>
2023-05-10 12:56:15 +0200

    add models

--- pkg/s3/model.go
+++ pkg/s3/model.go
@@ -1,1 +1,23 @@
 package s3
+
+import "time"
+
+type Bucket struct {
+	Name         string    `bson:"name"`
+	CreationDate time.Time `bson:"creation_date"`
+	Objects      []Object  `bson:"objects"`
+}
+
+type Object struct {
+	Key          string    `bson:"key"`
+	ETag         string    `bson:"etag"`
+	CreationDate time.Time `bson:"creation_date"`
+	LastModified time.Time `bson:"last_modified"`
+	Size         int64     `bson:"size"`
+}
+
+type BucketHandler interface {
+	CreateBucket(bucket Bucket) error
+	AddObject(bucketName string, object Object) error
+	GetObjects(bucketName string, max uint, prefix string, marker string)
+}
