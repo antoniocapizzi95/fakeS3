da72dec8ae8efb541101e1cf8e28ce955ef2c006
antonio <antoniocapizzi95@hotmail.it>
2023-05-11 18:48:47 +0200

    fix directories problem (small error)

--- utils/write_file.go
+++ utils/write_file.go
@@ -3,13 +3,12 @@ package utils
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 func WriteFile(basePath string, bucketName string, key string, file []byte) error {
 	entirePath := fmt.Sprintf("%s/%s/%s", basePath, bucketName, key)
-	dirPath := strings.Join(strings.Split(filepath.Dir(entirePath), "/")[:2], "/")
+	dirPath := getDirPath(entirePath)
 	err := createDirectoriesFromPath(dirPath)
 	if err != nil {
 		return fmt.Errorf("error creating folders: %s", err)
@@ -19,6 +18,11 @@ func WriteFile(basePath string, bucketName string, key string, file []byte) erro
 		return fmt.Errorf("error writing file: %s", err)
 	}
 	return nil
+}
+
+func getDirPath(path string) string {
+	directories := strings.Split(path, "/")
+	return strings.Join(directories[:len(directories)-1], "/")
 }
 
 func createDirectoriesFromPath(path string) error {
